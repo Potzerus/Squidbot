82d14de5c1199f20639bd1ee03ec8003dd7e275c
Potzerus <Potzerus@gmail.com>
2020-02-12 13:55:35 +0100

    Updating it to RE Bot's current level

--- Squid_Bot.py
+++ Squid_Bot.py
@@ -9,7 +9,17 @@ from tinydb import TinyDB, Query
 
 bot = commands.Bot(command_prefix="*#'")
 
+muted_role_id = 653898928726736907  # id for Lyds
 log_channel_id = 652847982399455262
+join_log_channel = 651787677498343464 # id for Lyds
+server = TinyDB("Data.json")
+
+
+
+def get_or_make_guild(server_id):
+    if not server.contains(Query().server == server_id):
+        server.insert({"server": server_id, "members": [], })
+    return server.search(Query().server == server_id)[0]
 
 
 @bot.event
@@ -19,10 +29,16 @@ async def on_ready():
     print("Logged in! bot invite: https://discordapp.com/api/oauth2/authorize?client_id=" +
           str(appli.id) + "&permissions=0&scope=bot")
 
+@bot.event
+async def on_bulk_message_delete(messages):
+    # Logging
+    for message in messages:
+        await on_message_delete(message)
 
 @bot.event
 async def on_message_delete(message):
-    embed = discord.Embed()
+    # Logging
+    embed = discord.Embed(color=discord.Color.red())
     embed.title = "Deleted Message"
     embed.add_field(name="Username", value=message.author)
     embed.add_field(name="UserId", value=message.author.id, inline=False)
@@ -33,48 +49,54 @@ async def on_message_delete(message):
 
 @bot.event
 async def on_message_edit(before, after):
+    # Logging
     if before.content != "" and before.content is not after.content:
-        embed = discord.Embed()
+        embed = discord.Embed(color=discord.Color.blue())
         embed.title = "Edited Message"
         embed.add_field(name="Username", value=after.author)
         embed.add_field(name="UserId", value=after.author.id, inline=False)
         embed.add_field(name="Channel", value="<#%d>" % before.channel.id, inline=False)
-        print(before.content)
         embed.add_field(name="Before", value=before.content, inline=False)
-        print(after.content)
         embed.add_field(name="After", value=after.content, inline=False)
-        print(embed)
         channel = bot.get_channel(log_channel_id)
         await channel.send(embed=embed)
 
 
-# muted_role_id = 0
-# server = TinyDB("Data.json")
-# def get_or_make_guild(server_id):
-#     if not server.contains(Query().server == server_id):
-#         server.insert({"server": server_id, "members": [], })
-#     return server.search(Query().server == server_id)[0]
+@bot.event
+async def on_member_remove(member):
+    # Sticky Roles
+    if member.guild.get_role(muted_role_id) in member.roles:
+        get_or_make_guild(member.guild.id)
+        members = server.all()[0]['members']
+        members.append(member.id)
+        server.update({"members": members}, Query().server == member.guild.id)
+
+    # Logging
+    embed = discord.Embed(color=discord.Color.orange())
+    embed.title = "User Left"
+    embed.add_field(name="Username", value=member)
+    embed.add_field(name="UserId", value=member.id, inline=False)
+    channel = bot.get_channel(join_log_channel)
+    await channel.send(embed=embed)
 
 
-# @bot.event
-# async def on_member_remove(member):
-#    if member.guild.get_role(muted_role_id) in member.roles:
-#        get_or_make_guild(member.guild.id)
-#        members = server.all()[0]['members']
-#        members.append(member.id)
-#        server.update({"members": members}, Query().server == member.guild.id)
-#        print(member.name + " caught leaving with a mute")
-#
-#
-# @bot.event
-# async def on_member_join(member):
-#    muted_members = get_or_make_guild(member.guild.id)['members']
-#    if member.id in muted_members:
-#        muted_members.remove(member.id)
-#        await member.add_roles(
-#            member.guild.get_role(muted_role_id), reason="Mute Persistence")
-#        server.update({"members": muted_members})
-#        print(member.name + " caught mute evading")
+@bot.event
+async def on_member_join(member):
+    # Sticky Roles
+    muted_members = get_or_make_guild(member.guild.id)['members']
+    if member.id in muted_members:
+        muted_members.remove(member.id)
+        await member.add_roles(
+            member.guild.get_role(muted_role_id), reason="Mute Persistence")
+        server.update({"members": muted_members})
+
+    # Logging
+    embed = discord.Embed(color=discord.Color.blue())
+    embed.title = "User Joined"
+    embed.add_field(name="Username", value=member)
+    embed.add_field(name="UserId", value=member.id, inline=False)
+    channel = bot.get_channel(join_log_channel)
+    await channel.send(embed=embed)
 
 
 bot.run(open("Squid-Token.txt").read())
